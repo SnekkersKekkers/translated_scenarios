e0705985d815f9387b842885b9926f7ca496154d
SnekkersKekkers <138995185+SnekkersKekkers@users.noreply.github.com>
2026-02-22 22:48:56 +0100

    Proofreading fix: t/t03_04_200_11.c (by Panda)

--- t/t03_04_200_11.c
+++ t/t03_04_200_11.c
@@ -5,7 +5,7 @@ MsgClear();
 MsgDisp("","At the vocational school, it seemed like
 people quickly found out that he was the
 model Nana. But ｛Nanatsumori＊｝ laughed
-about it when he talked about it.");
+when he talked about it.");
 MsgDisp("","Since he continues to live alone, I
 sometimes go over to his house to make
 meals and his favorite sweets, but
