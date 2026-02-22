e575f127516f344d9d73db9e6ccd59cf93baf4b0
SnekkersKekkers <138995185+SnekkersKekkers@users.noreply.github.com>
2026-02-22 17:22:36 +0100

    Proofreading fix: b/b04_02_061_02.c (by valentine)

--- b/b04_02_061_02.c
+++ b/b04_02_061_02.c
@@ -8,7 +8,7 @@ ScrFadeIn(0);
 VoicePlay("B040206102_02_000");
 MsgDisp("Sassa","I always get excited when I see those
 watermelon beach balls.");
-MsgSel("It's perfect for the beach","Makes me want to watermelon","The orca floaty would be good");
+MsgSel("It's perfect for the beach","Makes me want to eat watermelon","The orca floaty would be good");
 switch (MsgSelRsltGet()){
     case 0:
     DateRateSet(3);
