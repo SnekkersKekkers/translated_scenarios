93e48c6ac9fb8d15f720a5e09c26bf8438055d7c
SnekkersKekkers <138995185+SnekkersKekkers@users.noreply.github.com>
2026-02-24 09:26:04 +0100

    Proofreading fix: s/s01_03_002_02.c (by pistakyu) (#151)

--- s/s01_03_002_02.c
+++ s/s01_03_002_02.c
@@ -1,3 +1,3 @@
-MsgDisp("主人公","This one is ｛Honda＊＊｝'s . Wow...He wrote a
+MsgDisp("主人公","This one is ｛Honda＊＊｝'s. Wow...He wrote a
 lot of New Years trivia! I bet he put a
 lot of effort into it.");
