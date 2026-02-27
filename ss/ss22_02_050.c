4b40c825dc86c8eccd0b4a33e9cfe50b8c11b075
SnekkersKekkers <138995185+SnekkersKekkers@users.noreply.github.com>
2026-02-28 00:22:33 +0100

    Proofreading fix: ss/ss22_02_050.c (by pistakyu) (#268)

--- ss/ss22_02_050.c
+++ ss/ss22_02_050.c
@@ -3,5 +3,5 @@ ChEye(2,0);
 ChMouth(2,4);
 ChMotion(2,1);
 VoicePlay("SS2202050_02_000");
-MsgDisp("Sassa","Ole, I'm a cold weather person.");
+MsgDisp("Sassa","I'm a cold weather person.");
 MsgClose();
