21db25ab989791e07ea282c4e3e292bc5dda9007
SnekkersKekkers <138995185+SnekkersKekkers@users.noreply.github.com>
2026-02-28 23:32:50 +0100

    Proofreading fix: f/f01_02_000_02.c (by Snek) (#341)

--- f/f01_02_000_02.c
+++ f/f01_02_000_02.c
@@ -10,7 +10,7 @@ switch (MsgSelRsltGet()){
         BGMPlay("BGM_C02_SASSA_A",0.01);
         ChOpen(2,254,0,0,4,#1,#1,0,0);
         VoicePlay("F010200002_02_000");
-        MsgDisp("Sassa","Uh, what's up?");
+        MsgDisp("Sassa","Oh, what's up?");
         MsgDisp("主人公","I was thinking about walking home with
 you.");
         break ;
