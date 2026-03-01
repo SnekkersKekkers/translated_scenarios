1e31c31edff69d7a083f9240641e94ddb63d8f6f
SnekkersKekkers <138995185+SnekkersKekkers@users.noreply.github.com>
2026-03-01 13:14:45 +0100

    Proofreading fix: f/f03_21_000_01.c (by pistakyu) (#393)

--- f/f03_21_000_01.c
+++ f/f03_21_000_01.c
@@ -39,7 +39,7 @@ ChMouth(21,4);
 ChMotion(21,5);
 ChCheek(21,10);
 VoicePlay("F032100000_21_060");
-MsgDisp("Michiru","No way....this is so embarrassing.
+MsgDisp("Michiru","No way...this is so embarrassing.
 Can you let me off the hook already?");
 MsgDisp("主人公","(Hehe.
 ｛みちる＊｝ likes romance dramas, huh?)");
