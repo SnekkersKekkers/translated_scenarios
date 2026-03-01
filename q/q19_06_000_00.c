a13049330c05b71d86a25194fe1ac3dc89312d17
SnekkersKekkers <138995185+SnekkersKekkers@users.noreply.github.com>
2026-03-01 19:55:57 +0100

    Proofreading fix: q/q19_06_000_00.c (by Snek) (#484)

--- q/q19_06_000_00.c
+++ q/q19_06_000_00.c
@@ -14,7 +14,7 @@ SEStop("EV_SE_579",0);
 SEWait();
 MsgDisp("主人公","He, hello...?");
 VoicePlay("Q190600000_06_000");
-MsgDisp("Himuro","...Why are you answering.
+MsgDisp("Himuro","...Why did you pick up?
 Isn't it past lights out time?");
 MsgDisp("主人公","But ｛Himuro＊＊｝, you're the one who 
 called me...?");
