d3ce3a638992ef5414d859ed320dfb71ce3cc9a6
SnekkersKekkers <138995185+SnekkersKekkers@users.noreply.github.com>
2026-03-06 16:13:13 +0100

    Proofreading fix: a/a02_02_500_00.c (by Snek)

--- a/a02_02_500_00.c
+++ a/a02_02_500_00.c
@@ -77,7 +77,8 @@ ChEye(2,0);
 ChMouth(2,0);
 ChMotion(2,3);
 VoicePlay("A020250000_02_070");
-MsgDisp("Sassa","There was a girl like you...?\nI don't remember at all.");
+MsgDisp("Sassa","There was a girl like you...?
+I dont't remember at all.");
 ChEye(1,0);
 ChMouth(1,1);
 ChMotion(1,1);
@@ -139,7 +140,7 @@ ChEye(2,0);
 ChMouth(2,3);
 ChMotion(2,3);
 VoicePlay("A020250000_02_120");
-MsgDisp("Sassa","I completely forgot!");
+MsgDisp("Sassa","Forget about that for now!");
 ChEye(1,0);
 ChMouth(1,2);
 BGMVol(0.5,0.1);
