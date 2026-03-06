529b9d7b21d191899c21c61a53e736d483f2048d
SnekkersKekkers <138995185+SnekkersKekkers@users.noreply.github.com>
2026-03-06 15:58:33 +0100

    Proofreading fix: ap/ap06_08_300.c (by Snek)

--- ap/ap06_08_300.c
+++ ap/ap06_08_300.c
@@ -2,7 +2,5 @@ NSSEye(8,4);
 NSSMouth(8,3);
 NSSCheek(8,5);
 VoicePlay("AP0608300_08_000");
-MsgDisp("Shirahane","Naa.
-This is what they call getting hot and
-heavy, ain't it?");
+MsgDisp("Shirahane","Y'know.\nThis is what they call flirtin', ain't it?");
 MsgClose();
