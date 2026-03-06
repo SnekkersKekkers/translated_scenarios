5d40d187de2bd4e054f8dc9c4f24f1a4793a7d52
SnekkersKekkers <138995185+SnekkersKekkers@users.noreply.github.com>
2026-03-06 16:06:41 +0100

    Proofreading fix: ap/ap01_02_100.c (by Snek) (#593)

--- ap/ap01_02_100.c
+++ ap/ap01_02_100.c
@@ -1,6 +1,5 @@
 NSSEye(2,0);
 NSSMouth(2,4);
 VoicePlay("AP0102100_02_000");
-MsgDisp("Sassa","Feels like I had a number done on me but
-just keep bringing it on next time too.");
+MsgDisp("Sassa","It was kind of chaotic, but let's come\nagain sometime.");
 MsgClose();
