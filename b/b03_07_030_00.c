bc82bccd311d354a6ec4bfd4ac3136e67c6c3f66
SnekkersKekkers <138995185+SnekkersKekkers@users.noreply.github.com>
2026-03-10 18:32:43 +0100

    wrapping fix

--- b/b03_07_030_00.c
+++ b/b03_07_030_00.c
@@ -7,8 +7,8 @@ SEPlay("EV_SE_566",0.3);
 ScrFadeIn(0);
 MsgDispSksp(1,0);
 VoicePlay("B030703000_07_000");
-MsgDisp("Mikage","Alright, today's extracurricular activity is at the
-aquarium.
+MsgDisp("Mikage","Alright, today's extracurricular activity is 
+at the aquarium.
 Don't get too wild, you guys.");
 VoicePlay("B030703000_42_000");
 MsgDisp("Male Student","Okaay.");
