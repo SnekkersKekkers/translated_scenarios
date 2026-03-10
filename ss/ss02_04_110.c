2659dd38bea386d20ec9831436c6d2673cf3bcba
SnekkersKekkers <138995185+SnekkersKekkers@users.noreply.github.com>
2026-03-10 22:10:22 +0100

    Proofreading fix: ss/ss02_04_110.c (by Snek)

--- ss/ss02_04_110.c
+++ ss/ss02_04_110.c
@@ -3,5 +3,5 @@ ChMouth(4,1);
 ChMotion(4,1);
 ChEyeOpenLevel(4,8);
 VoicePlay("SS0204110_04_000");
-MsgDisp("Nanatsumori","Stop the prank.");
+MsgDisp("Nanatsumori","Stop messing around.");
 MsgClose();
